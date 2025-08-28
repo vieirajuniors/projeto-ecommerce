1a93faebb812c632dd0b29ff214a4faa6efe17d5
Jorge Júnior <vieirajuniors@outlook.com>
2025-08-28 19:20:28 -0300

    reorganização das pastas do projeto

---
+++ js/carrinho.js
@@ -0,0 +1,16 @@
+/*
+Objetivo 1 - quando clicar no botão de adicionar ao carrinho:
+    - atualizar o contador
+    - adicionar o produto no localStorage
+    - atualizar a tabela HTML do carrinho
+
+Objetivo 2 - remover produtos do carrinho:
+    - ouvir o botão de deletar
+    - remover do localStorage
+    - atualizar o DOM e o total
+
+Objetivo 3 - atualizar valores do carrinho:
+    - ouvir mudanças de quantidade
+    - recalcular total individual
+    - recalcular total geral
+*/
