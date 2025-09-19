e9ee63e5548eed7bacb232625a679bd3e75df933
Jorge Júnior <vieirajuniors@outlook.com>
2025-09-19 16:11:05 -0300

    atualiza o contador do carrinho

--- js/carrinho.js
+++ js/carrinho.js
@@ -58,6 +58,7 @@ botoesAdicionarAoCarrinho.forEach(botao => {
         }
 
         salvarProdutosNoCarrinho(carrinho);
+        atualizarContadorCarrinho();
     });
 });
 
@@ -69,3 +70,17 @@ function obterProdutosDoCarrinho() {
     const produtos = localStorage.getItem("carrinho");
     return produtos ? JSON.parse(produtos) : [];
 }
+
+// passo 4- atualizar o contador do carrinho de compras
+function atualizarContadorCarrinho() {
+    const carrinho = obterProdutosDoCarrinho();
+    let total = 0;
+
+    carrinho.forEach(produto => {
+        total += produto.quantidade;
+    });
+
+    document.getElementById("contador-carrinho").textContent = total;
+}
+
+atualizarContadorCarrinho();
