5e0aa5ae578922b2a967550f7828b23d4487775b
Jorge Júnior <vieirajuniors@outlook.com>
2025-09-19 16:50:07 -0300

    renderiza a tabela do carrinho e muda a cor da última caneca da lista de produtos

--- js/carrinho.js
+++ js/carrinho.js
@@ -59,6 +59,7 @@ botoesAdicionarAoCarrinho.forEach(botao => {
 
         salvarProdutosNoCarrinho(carrinho);
         atualizarContadorCarrinho();
+        renderizarTabelaDoCarrinho();
     });
 });
 
@@ -73,10 +74,10 @@ function obterProdutosDoCarrinho() {
 
 // passo 4- atualizar o contador do carrinho de compras
 function atualizarContadorCarrinho() {
-    const carrinho = obterProdutosDoCarrinho();
+    const produtos = obterProdutosDoCarrinho();
     let total = 0;
 
-    carrinho.forEach(produto => {
+    produtos.forEach(produto => {
         total += produto.quantidade;
     });
 
@@ -84,3 +85,28 @@ function atualizarContadorCarrinho() {
 }
 
 atualizarContadorCarrinho();
+
+// passo 5- renderizar a tabela do carrinho de compras
+function renderizarTabelaDoCarrinho() {
+    const produtos = obterProdutosDoCarrinho();
+    const corpoTabela = document.querySelector("#modal-1-content table tbody");
+    corpoTabela.innerHTML = ""; // Limpar tabela antes de renderizar
+
+    produtos.forEach(produto => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `<td class="td-produto">
+                        <img 
+                            src="${produto.imagem}"
+                            alt="${produto.nome}"
+                        />
+                        </td>
+                        <td>${produto.nome}</td>
+                                <td class="td-preco-unitario">R$ ${produto.preco.toFixed(2).replace(".", ",")}</td>
+                                <td class="td-quantidade">
+                                <input type="number" value="${produto.quantidade}" min="1"></td>
+                                <td class="td-preco-total">R$ ${produto.preco.toFixed(2).replace(".", ",")}</td>
+                                <td><button class="btn-remover" data-id="${produto.id}" id="deletar"></button></td>`;
+        corpoTabela.appendChild(tr);
+    });
+}
+renderizarTabelaDoCarrinho();
